533e0eacbe52264167f462ad83f1ba49c3bd8f22
jl <jonathan.arthur.lock@gmail.com>
2018-11-04 21:44:29 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # chatter
+
+
+Installation Instructions:
+Remove Pusher keys in admin.js and app.js
+
+Set environment variables:
+PUSHER_APP_ID
+PUSHER_APP_KEY
+PUSHER_APP_SECRET
+($ export <var>=<value>)
---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, request, json
+from flask_cors import CORS
+from pusher import pusher
+# import simplejson
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+PUSHER_APP_ID = os.environ['PUSHER_APP_ID']
+PUSHER_APP_KEY = os.environ['PUSHER_APP_KEY']
+PUSHER_APP_SECRET = os.environ['PUSHER_APP_SECRET']
+PUSHER_APP_CLUSTER = os.environ['PUSHER_APP_CLUSTER']
+
+pusher = pusher_client = pusher.Pusher(
+                                       app_id=PUSHER_APP_ID,
+                                       key=PUSHER_APP_KEY,
+                                       secret=PUSHER_APP_SECRET,
+                                       cluster=PUSHER_APP_CLUSTER,
+                                       ssl=True
+                                       )
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html', PUSHER_APP_KEY=PUSHER_APP_KEY)
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', PUSHER_APP_KEY=PUSHER_APP_KEY)
+
+
+@app.route('/new/guest', methods=['POST'])
+def guestUser():
+    data = request.json
+
+    pusher.trigger(u'general-channel', u'new-guest-details', {
+        'name': data['name'],
+        'email': data['email']
+    })
+
+    return json.dumps(data)
+
+
+@app.route("/pusher/auth", methods=['POST'])
+def pusher_authentication():
+    auth = pusher.authenticate(
+                              channel=request.form['channel_name'],
+                              socket_id=request.form['socket_id']
+                              )
+    return json.dumps(auth)
+
+
+if __name__ == '__main__':
+    app.run()
